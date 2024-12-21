d0e4551d102ccf92f1e8dccd0c661c86630c0383
Wingleszy <meowingless@mail.ru>
2024-12-21 05:25:03 +0300

    init

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+
+import { Controls } from '../features/controls/Controls';
+import { CountryList } from '../features/countries/CountryList';
+
+export const HomePage = () => {
+ 
+
+  return (
+    <>
+      <Controls />
+      <CountryList />
+    </>
+  );
+};
